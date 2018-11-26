50f40159b8d6f4d44a43985e293178378dd449cb
Blake Mason <blakelmason@gmail.com>
2018-11-26 15:30:16 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
